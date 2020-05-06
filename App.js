df7832606dbfb91181d5dc4bd15e9e4b9b90b475
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-05-06 18:35:52 +0600

    Switch to Bare Workflow from Managed Workflow

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native'
+import Geolocation from '@react-native-community/geolocation'
+import { Socket } from './phoenix'
 
-export default function App() {
+async function requestLocationPermission (cb) {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location Permission',
+        message: 'This App needs access to your location ' +
+                   'so we can know where you are.'
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      cb()
+    } else {
+      console.log('Location permission denied')
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+}
+
+function pushLocationUpdate (channel, socketStatus) {
+  if (socketStatus) {
+  // Fetch location info
+    requestLocationPermission(() => {
+      Geolocation.getCurrentPosition(info => {
+        const body = {
+          altitude: info.coords.altitude,
+          longitude: info.coords.longitude,
+          latitude: info.coords.latitude,
+          timestamp: info.timestamp
+        }
+
+        channel.push('location_update', body)
+      })
+    })
+  }
+}
+
+export default function App () {
+  const socket = new Socket('http://192.168.0.100:4000/socket')
+
+  let socketStatus = 0
+  socket.onOpen(event => {
+    const channel = socket.channel('room:lobby', {})
+    channel.join()
+      .receive('ok', resp => {
+        socketStatus = 1
+
+        pushLocationUpdate(channel, socketStatus)
+        window.setInterval(() => {
+          pushLocationUpdate(channel, socketStatus)
+        }, 5000)
+      })
+      .receive('error', resp => {
+        socketStatus = 0
+      })
+
+    pushLocationUpdate(channel)
+  })
+  socket.onClose(() => {
+    socketStatus = 0
+  })
+  socket.onError(() => {
+    socketStatus = 0
+  })
+  socket.connect()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Suck my ass!</Text>
+      <Text>Fuck my ass!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +83,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
